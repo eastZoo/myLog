90e7e7a304b7ea5ac818630a30ca65c10f3814d7
DESKTOP-U8HSDMJ\dongj <dongju010@naver.com>
2022-03-23 15:43:51 +0900

    feat : saga&reducer link

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,23 +1,25 @@
 export const initialState = {
+    isLoggingIn: false,  // 로그인 시도중 true면 로딩창을 띄우는 용도
     isLoggedIn: false,
+    isLoggingOut: false, // 로그아웃 시도중 true면 로딩창을 띄우는 용도
     me: null,
     signUpData: {},
     loginData: {},
 };
 
-export const  loginAction = (data) => {
-    return (dispatch, getState) => {
-        const state = getState();
-        dispatch(loginRequestAction());
-        axios.post('/api/login')
-            .then((res) => {
-                dispatch(loginSuccessAction(res.data));
-            })
-            .catch((err) => {
-                dispatch(loginFailureAction(err));
-            })
-    }
-}
+// export const  loginAction = (data) => {
+//     return (dispatch, getState) => {
+//         const state = getState();
+//         dispatch(loginRequestAction());
+//         axios.post('/api/login')
+//             .then((res) => {
+//                 dispatch(loginSuccessAction(res.data));
+//             })
+//             .catch((err) => {
+//                 dispatch(loginFailureAction(err));
+//             })
+//     }
+// }
 
 export const loginRequestAction = (data) => {
     return {
@@ -60,18 +62,42 @@ export const logoutFailureAction = (data) => {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'LOG_IN':
+        case 'LOG_IN_REQUEST':
             return {
                 ...state,
+                isLoggingIn: true,
+            };
+        case 'LOG_IN_SUCCESS':
+            return {
+                ...state,
+                isLoggingIn: false,
                 isLoggedIn: true,
                 me: action.data,
             };
-        case 'LOG_OUT':
+        case 'LOG_IN_FAILURE':
             return {
                 ...state,
+                isLoggingIn: false,
+                isLoggedIn: false,
+            };
+        case 'LOG_OUT_REQUEST':
+            return {
+                ...state,
+                isLoggingOut: true, //request일때는 ing true
+            };
+        case 'LOG_OUT_SUCCESS':
+            return {
+                ...state,
+                isLoggingOut: false, //로그인 성공 시 ing false
                 isLoggedIn: false,
                 me: null,
             };
+        case 'LOG_OUT_FAILURE':
+            return {
+                ...state,
+                isLoggingOut: false, //실패해도 ing는 false
+            };
+
         default:
             return state;
     }
