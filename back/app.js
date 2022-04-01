72424e8302c3da171ef2d6c54a354b68f783a2a5
DESKTOP-U8HSDMJ\dongj <dongju010@naver.com>
2022-04-02 00:39:58 +0900

    feat: add signup function logic(use bcrypt)

--- back/app.js
+++ back/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
 const db = require('./models');
 const app = express();
 //서버 실행시 디비 시퀄라이즈 연결도 같이하는 코드
@@ -8,6 +9,10 @@ db.sequelize.sync()
         console.log('db 연결 성공');
     })
     .catch(console.error);
+
+//front에서 보내준 데이터를 req.body 안에 넣어주는 역할!!!
+app.use(express.json());  //front에서 json형식 파일 req.body안에 넣어줌
+app.use(express.urlencoded({ extended: true})); //front에서 form submit형식 파일 req.body안에 넣어줌
 
 app.get('/', (req, res) => {
     res.send('hello express')
@@ -26,6 +31,7 @@ app.get('/posts', (req, res) => {
 });
 
 app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.listen(3065, () => {
     console.log('서버 실행 중!!');
---
+++ back/routes/user.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const { User } = require('../models');
+
+const router = express.Router();
+
+// start signUp course #5
+//req <- from saga/user.js signUpAPI data
+router.post('/', async (req, res, next) => { //POST /user
+    try{
+        // 중복회원 검사
+        const exUser = await User.findOne({
+            where: {
+                email: req.body.email,
+            }
+        });
+        if (exUser) {
+            return res.status(403).send("already use email.") // return 안쓰면 밑에코드까지 그대로 실행되고 res 응답 두번 보내져 error 발생( 응답은 무조건 1번 )
+        }
+        //bcrypt도 비동기라 await 붙여줌
+        const hashedPassword = await bcrypt.hash(req.body.password, 10); //보통 10~13 넣음
+        await User.create({
+            email : req.body.email,
+            nickname:  req.body.nickname,
+            password: hashedPassword,
+        });
+        res.status(200).send('ok');
+    } catch (error) {
+        console.log(error);
+        next(error); //status 500
+    }
+});
+
+module.exports = router;
