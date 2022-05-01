a9a8ad9c232fefc80f4b389becf6090c685b8fa7
DESKTOP-U8HSDMJ\dongj <dongju010@naver.com>
2022-05-02 01:02:25 +0900

    update : origin edit

--- back/app.js
+++ back/app.js
@@ -30,7 +30,7 @@ passportConfig();
 if (process.env.NODE_ENV === 'production') {
     app.use(morgan('combined'));
     app.use(hpp());
-    app.use(helmet({ contentSecurityPolicy: false }));
+    app.use(helmet());
     app.use(cors({
         origin: 'http://eastzoo.co.kr',
         credentials: true,
